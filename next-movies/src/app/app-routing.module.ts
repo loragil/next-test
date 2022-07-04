a93430917125757a64c1de18f2f4504d5e5596c5
loragil <loragil@gmail.com>
2022-07-04 12:58:33 +0200

    added HOME route & base component + additional WIP

--- next-movies/src/app/app-routing.module.ts
+++ next-movies/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+// import { HomeModule } from './pages/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    // { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+    { path: 'home', component: HomeComponent },
+    { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    // imports: [RouterModule],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
